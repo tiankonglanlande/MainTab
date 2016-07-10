d4fe31d625479d9902a834d5fc01919e6ca0ef27
tiankonglanlande <zswdxl_111@sina.com>
2016-07-10 21:44:01 +0800

    第一次提交
    完成主界面功能

---
+++ app/src/main/java/com/example/administrator/maintab/MainTabActivity.java
@@ -0,0 +1,147 @@
+package com.example.administrator.maintab;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainTabActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+    private ViewPager mViewPager;
+    int[]btIds={R.id.bt,R.id.bt2,R.id.bt3,R.id.bt4};
+    ArrayList<ImageButton> ivList;
+    ArrayList<Fragment> mainTabFgList;
+    private PagerAdapter pagetAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intView();
+        setListener();
+    }
+    private void setListener() {
+        for (int i = 0; i < ivList.size(); i++) {
+            final ImageButton iv=ivList.get(i);
+            final int index=i;
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!iv.isSelected()){
+                        resetIvBtnSelect();
+                        selectIvBtnSelect(index);
+                        mViewPager.setCurrentItem(index);
+                    }
+                }
+            });
+        }
+    }
+
+    public void resetIvBtnSelect() {
+        for (int a=0;a<ivList.size();a++){
+            ImageButton ivOther=ivList.get(a);
+            ivOther.setSelected(false);
+        }
+    }
+
+    private void intView() {
+        ivList=new ArrayList<ImageButton>();
+        mainTabFgList=new ArrayList<Fragment>();
+        mViewPager= (ViewPager) findViewById(R.id.container);
+        for (int i=0;i<btIds.length;i++){
+            ImageButton iv=(ImageButton)findViewById(btIds[i]);
+            if (i==0){
+                iv.setSelected(true);
+            }else{
+                iv.setSelected(false);
+            }
+            ivList.add(iv);
+        }
+        for (int i=0;i<ivList.size();i++){
+            mainTabFgList.add(new OneFragment());
+        }
+        pagetAdapter=new PagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(pagetAdapter);
+        mViewPager.addOnPageChangeListener(this);
+        resetTabAphal();
+        selectTabAphal(0);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        resetTabAphal();
+        selectTabAphal(position);
+        resetIvBtnSelect();
+        selectIvBtnSelect(position);
+    }
+
+    private void selectIvBtnSelect(int position) {
+        ImageButton ivbtn=ivList.get(position);
+        ivbtn.setSelected(true);
+    }
+
+    private void resetTabAphal() {
+        for (ImageButton iv:ivList){
+            iv.setAlpha(0.5f);
+        }
+    }
+    private void selectTabAphal(int position) {
+        for (int i=0;i<ivList.size();i++){
+            if (position==i){
+                ivList.get(i).setAlpha(1.0f);
+
+            }
+        }
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+
+    class PagerAdapter extends FragmentPagerAdapter{
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mainTabFgList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mainTabFgList.size();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/administrator/maintab/OneFragment.java
@@ -0,0 +1,94 @@
+package com.example.administrator.maintab;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.plus.PlusOneButton;
+
+import java.util.ArrayList;
+
+/**
+ * A fragment with a Google +1 button.
+ */
+public class OneFragment extends Fragment {
+
+    private ViewPager pager;
+    ArrayList<Fragment> fgTabFgList;
+    private View view;
+    private InnerPagerAdapter adapter;
+    String[]titles={"首页","前端","Android","ios"};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_one, container, false);
+        intView();
+        return view;
+    }
+
+    private void intView() {
+        pager= (ViewPager) view.findViewById(R.id.pager);
+        fgTabFgList=new ArrayList<Fragment>();
+        for (int i=0;i<4;i++){
+            fgTabFgList.add(new MyFragment());
+        }
+        adapter=new InnerPagerAdapter(getChildFragmentManager());
+        pager.setAdapter(adapter);
+
+        TabLayout tablayout=(TabLayout)view.findViewById(R.id.tabs);
+        tablayout.setTabTextColors(getResources().getColor(R.color.gray),getResources().getColor(R.color.blue));
+        tablayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.blue));
+        tablayout.setupWithViewPager(pager);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+
+    class InnerPagerAdapter extends FragmentPagerAdapter {
+
+        public InnerPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fgTabFgList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fgTabFgList.size();
+        }
+        public CharSequence getPageTitle(int position){
+            String title=titles[position];
+            return title;
+        }
+    }
+
+    public static class MyFragment extends Fragment{
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_inner, container, false);
+            return view;
+        }
+    }
+
+
+}
